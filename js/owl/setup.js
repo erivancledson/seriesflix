d37e73af6636bf18d4c3cf8308ab492cfd694789
Erivan Cledson <erivancled@hotmail.com>
2021-03-28 19:25:02 -0300

    seriesflix

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true, //se colocar false ele não cria o loop
+    margin:10,
+    nav:false, //se colocar true ele exibe setas de navegação
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
