40c958a044dc5906c701d4e17bc2f30b80035993
Erivan Cledson <erivancled@hotmail.com>
2021-04-04 18:35:00 -0300

    adicione auto play no carrosel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -2,6 +2,8 @@ $('.owl-carousel').owlCarousel({
     loop:true, //se colocar false ele não cria o loop
     margin:10,
     nav:false, //se colocar true ele exibe setas de navegação
+    autoplay:true,
+    autoplayTimeout:1000,
     responsive:{
         0:{
             items:1
